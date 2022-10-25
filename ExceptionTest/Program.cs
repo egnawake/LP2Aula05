a1cc338570033ad5597867a534846d620e1221ec
João Inácio <egnawake@gmail.com>
2022-10-25 12:35:42 +0100

    Test generating an exception

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -6,7 +6,9 @@ namespace ExceptionTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Insere um numero inteiro: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Numero inserido: {i}");
         }
     }
 }
