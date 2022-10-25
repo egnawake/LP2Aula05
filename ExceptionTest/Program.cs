4fada14c0228c67f079e1833a4c5e745bd144c2d
João Inácio <egnawake@gmail.com>
2022-10-25 12:55:50 +0100

    Catch specific exceptions

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -12,9 +12,17 @@ namespace ExceptionTest
                 int i = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine($"Numero inserido: {i}");
             }
+            catch (FormatException)
+            {
+                Console.WriteLine("O valor inserido nao e um numero inteiro");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("O valor inserido e um numero demasiado grande");
+            }
             catch (Exception)
             {
-                Console.WriteLine("O valor inserido e invalido");
+                Console.WriteLine("Ocorreu um erro");
             }
         }
     }
