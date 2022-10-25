8df83fb0b7421e8db070c73952c62317af6561b5
João Inácio <egnawake@gmail.com>
2022-10-25 12:48:12 +0100

    Catch exception and show error message

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -7,8 +7,15 @@ namespace ExceptionTest
         static void Main(string[] args)
         {
             Console.Write("Insere um numero inteiro: ");
-            int i = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Numero inserido: {i}");
+            try
+            {
+                int i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Numero inserido: {i}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("O valor inserido e invalido");
+            }
         }
     }
 }
